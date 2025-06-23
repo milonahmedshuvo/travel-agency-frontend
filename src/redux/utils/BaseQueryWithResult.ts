8eeee504e20a5fb1c1896616eae4e1d84e3dfef5
euhansarkar <euhansarkar@gmail.com>
2025-06-24 02:53:29 +0600

    Solved: Base Api Redifined.

--- src/redux/utils/BaseQueryWithResult.ts
+++ src/redux/utils/BaseQueryWithResult.ts
@@ -5,50 +5,42 @@ import type {
   FetchArgs,
   FetchBaseQueryError,
 } from "@reduxjs/toolkit/query";
+import { getBaseUrl } from "@/config/base-url";
 // import Cookies from "js-cookie";
 // import { logout } from "../slice/auth/authSlice";
 
-const baseUrl = process.env.NEXT_PUBLIC_API_URL
-
+const baseUrl = getBaseUrl();
 
 const rawBaseQuery = fetchBaseQuery({
   baseUrl,
   // credentials: "include",
 
-  
   prepareHeaders: (headers) => {
     // const token = Cookies.get("token");
-    const token = localStorage.getItem('token')
+    const token = localStorage.getItem("token");
     if (token) {
-      headers.set("authorization",  `Bearer ${token}`);
+      headers.set("authorization", `Bearer ${token}`);
     }
     headers.set("accept", "application/json");
     return headers;
   },
 });
 
-
-
-
-
-
 export const baseQueryWithReauth: BaseQueryFn<
   string | FetchArgs,
   unknown,
   FetchBaseQueryError
 > = async (args, api, extraOptions) => {
-
-//  args : body: data 
-        // method: post 
-        // url: ''
+  //  args : body: data
+  // method: post
+  // url: ''
 
   let result = await rawBaseQuery(args, api, extraOptions);
   console.log(result.error, "ree ref");
-  console.log({args, api, extraOptions})
-  console.log({result})
+  console.log({ args, api, extraOptions });
+  console.log({ result });
 
-
-  //  if have not token in requst headers, then show error  
+  //  if have not token in requst headers, then show error
   if (result.error && result.error.status === 401) {
     // Attempt to get a new access token
     const refreshResult = await rawBaseQuery(
@@ -58,27 +50,24 @@ export const baseQueryWithReauth: BaseQueryFn<
     );
     console.log(refreshResult, "ll");
 
-
     if (refreshResult.data) {
       const newAccessToken = (refreshResult.data as any)?.data?.accessToken;
       // Save new access token in cookies
-    //   Cookies.set("token", newAccessToken);
-        localStorage.setItem('token', newAccessToken)
-         
+      //   Cookies.set("token", newAccessToken);
+      localStorage.setItem("token", newAccessToken);
 
-      
       // Retry the original query with new token
       result = await rawBaseQuery(args, api, extraOptions);
     } else {
       // Refresh token failed: log out or handle error
       // Cookies.remove("refreshToken");
       // Cookies.remove("token");
-      localStorage.removeItem('token')
+      localStorage.removeItem("token");
 
       // Optionally trigger logout in redux
-    //   api.dispatch(logout());
+      //   api.dispatch(logout());
 
-    //   window.location.href = "/signin";
+      //   window.location.href = "/signin";
     }
   }
 
