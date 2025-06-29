0a0f2bae4863fceaab8963e7be8dbce212eb56f6
euhansarkar <euhansarkar@gmail.com>
2025-06-29 23:50:52 +0600

    Added: Sitemap Generator.

--- src/scripts/sitemap-generator.js
+++ src/scripts/sitemap-generator.js
@@ -1,20 +1,20 @@
-const { SitemapStream, streamToPromise } = require("sitemap");
-const { createWriteStream } = require("fs");
+// const { SitemapStream, streamToPromise } = require("sitemap");
+// const { createWriteStream } = require("fs");
 
-const sitemap = new SitemapStream({ hostname: "https://hvarlocalguide.com" });
+// const sitemap = new SitemapStream({ hostname: "https://hvarlocalguide.com" });
 
-const writeStream = createWriteStream("./public/sitemap.xml");
+// const writeStream = createWriteStream("./public/sitemap.xml");
 
-// List of static URLs
-const urls = [
-  { url: "/", changefreq: "daily", priority: 1.0 },
-  { url: "/about", changefreq: "monthly" },
-  { url: "/contact", changefreq: "monthly" },
-];
+// // List of static URLs
+// const urls = [
+//   { url: "/", changefreq: "daily", priority: 1.0 },
+//   { url: "/about", changefreq: "monthly" },
+//   { url: "/contact", changefreq: "monthly" },
+// ];
 
-urls.forEach((url) => sitemap.write(url));
-sitemap.end();
+// urls.forEach((url) => sitemap.write(url));
+// sitemap.end();
 
-streamToPromise(sitemap).then((data) => {
-  writeStream.write(data.toString());
-});
+// streamToPromise(sitemap).then((data) => {
+//   writeStream.write(data.toString());
+// });
